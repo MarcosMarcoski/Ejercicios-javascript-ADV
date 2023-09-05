24dc134df26e825a9c050549b3c76c0f483623cf
Marcoski <marcosdediego@gmail.com>
2023-09-05 13:59:25 +0200

    ejercicio 3

---
+++ Ejercicios devoluciones de llamada/Ejercicio 3/ejercicio3.js
@@ -0,0 +1,16 @@
+const repeatHello = (callback) => {
+    const intervalId = setInterval(() => {
+      callback();
+    }, 1000);
+  
+    setTimeout(() => {
+      clearInterval(intervalId);
+    }, 5000);
+  };
+  
+  const helloCallback = () => {
+    console.log("Hola");
+  };
+  
+  repeatHello(helloCallback);
+  
